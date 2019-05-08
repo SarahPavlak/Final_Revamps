5580ce5786b5e066b1c5f9f99e56746e033f4afc
SarahPavlak <scp61@georgetown.edu>
2019-05-08 16:53:15 -0400

    setting up executive dashboard

---
+++ Shopping_Cart/test/my_test.py
@@ -0,0 +1,5 @@
+from app.my_shopping_test import enlarge 
+
+def test_enlarge(): 
+    result = enlarge(3) 
+    assert result == 300 
--- test/my_test.py
+++
@@ -1,5 +0,0 @@
-from app.my_script import enlarge # load the `enlarge()` function to avoid NameError: name 'enlarge' is not defined
-
-def test_enlarge(): # note the function name is prefixed with "test_"
-    result = enlarge(3) # directly invoke the function we want to test
-    assert result == 300 # describe expectations for desired behavior
