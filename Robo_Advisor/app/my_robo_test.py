9aae1aca6cdd420b451bb5d740ac21d4d75aa3a5
SarahPavlak <scp61@georgetown.edu>
2019-05-09 14:02:48 -0400

    successful url pytest

--- Robo_Advisor/app/my_robo_test.py
+++ Robo_Advisor/app/my_robo_test.py
@@ -2,3 +2,7 @@
 
 def to_usd(i):
     return "${0:,.2f}".format(i)
+
+def compile_url():
+    user_input = "MSFT"
+    return "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + user_input + "&apikey=" + "API_KEY"
--- Robo_Advisor/test/my_test.py
+++ Robo_Advisor/test/my_test.py
@@ -1,4 +1,4 @@
-from app.my_robo_test import to_usd
+from app.my_robo_test import to_usd, compile_url
 
 def test_to_usd():
     result = to_usd(4)
@@ -9,3 +9,8 @@ def test_to_usd():
 
     result = to_usd(1057.70)
     assert result == '$1,057.70'
+
+def test_compile_url():
+    result = compile_url()
+    assert result == "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=API_KEY"
+
