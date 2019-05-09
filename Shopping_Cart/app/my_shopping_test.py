37d3ba4d411ac828f1e4361bf22eff650e47cdca
SarahPavlak <scp61@georgetown.edu>
2019-05-09 11:10:24 -0400

    running timestamp test

--- Shopping_Cart/app/my_shopping_test.py
+++ Shopping_Cart/app/my_shopping_test.py
@@ -1,6 +1,10 @@
-
+import datetime
 #My Tests
 
 #Testing that formula turns it into nicely formatted number with $ and two decimal places
 def to_usd(i):
     return "${0:,.2f}".format(i)
+
+def human_friendly_timestamp():
+    t = datetime.datetime(2012, 2, 23, 0, 0)
+    return t.strftime('%m/%d/%Y')
--- Shopping_Cart/test/my_test.py
+++ Shopping_Cart/test/my_test.py
@@ -1,4 +1,5 @@
-from app.my_shopping_test import to_usd
+from app.my_shopping_test import to_usd, human_friendly_timestamp
+import datetime 
 
 
 def test_to_usd():
@@ -10,3 +11,11 @@ def test_to_usd():
 
     result = to_usd(1057.70)
     assert result == '$1,057.70'
+
+def test_human_friendly_timestamp():
+    t = datetime.datetime(2012, 2, 23, 0, 0)
+    result = human_friendly_timestamp()
+    assert result == t.strftime('%m/%d/%Y')
+
+
+
