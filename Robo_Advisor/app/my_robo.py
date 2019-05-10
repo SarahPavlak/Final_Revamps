8817bf93fa633595f5837bd3c5a0bd95d10c0e5c
SarahPavlak <46604461+SarahPavlak@users.noreply.github.com>
2019-05-10 18:24:03 -0400

    Update my_robo.py
    redid risk recommendation to be more streamlined and make more sense

--- Robo_Advisor/app/my_robo.py
+++ Robo_Advisor/app/my_robo.py
@@ -13,49 +13,36 @@ def to_usd(my_price):
 
 API_KEY = os.environ.get('MY_API_KEY')
 
-
 #Collecting User Information
 user_input = input ("Please print a stock symbol: ")
-symbol = user_input 
-
-#Obtaining the desired stock information
-print("-----------------------------------------------------")
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + user_input + "&apikey=" + API_KEY
-print("Your desired stock information comes from:")
-print(request_url)
-print("-----------------------------------------------------")
-
-request = request_url
+print("Your desired info comes from: " + request_url)
 response = requests.get(request_url)
 
 #Validating inputs
-
 while True:     
         if user_input.isalpha() and len(user_input) < 6 : 
-                print(user_input)
-                
-                if 'Error' in response.text: #this line adapted from https://github.com/hiepnguyen034/robo-stock/blob/master/robo_advisor.py
-                        print('The stock you are looking for is not here. The program will now close.')
-                        exit()
-                break
+            if 'Error' in response.text: #this line adapted from https://github.com/hiepnguyen034/robo-stock/blob/master/robo_advisor.py
+                    print('The stock you are looking for is not here. The program will now close.')
+                    exit()
+            break
         else: 
                 print("Oh, expecting a properly-formed stock symbol like 'MSFT' with no more than six symbols. Please try again. The program will now exit!")
                 exit()
         break
         
-        
         #source: https://stackoverflow.com/questions/36432954/python-validation-to-ensure-input-only-contains-characters-a-z
         #source: https://stackoverflow.com/questions/8761778/limiting-python-input-strings-to-certain-characters-and-lengths 
   
 
-
 parsed_response = json.loads(response.text) #from class
 tsd = parsed_response["Time Series (Daily)"] #from screencast
 dates = list(tsd.keys()) #from screencast
+
+#defining variables
 latest_day = dates[0] #from screencast
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"] 
 latest_close_usd = tsd[latest_day]["4. close"] #from screencast
-
 
 high_prices = [] 
 for date in dates:
@@ -71,73 +58,28 @@ for date in dates:
 
 recent_low = min(low_prices)
 
-csv_file_path = os.path.join (os.path.dirname(__file__), "data", "prices_" + user_input + ".csv")
-csv_header = ["timestamp", "open", "low", "high", "close", "volume"]
-
-with open(csv_file_path, "w") as csv_file:
-        writer = csv.DictWriter (csv_file, fieldnames = csv_header)
-
-        for date in dates:
-            daily_prices = tsd[date]
-
-            writer.writerow({
-                "timestamp": date,  
-                "open": daily_prices["1. open"],
-                "high": daily_prices["2. high"], 
-                "low": daily_prices["3. low"],
-                "close": daily_prices["4. close"], 
-                "volume": daily_prices["5. volume"] 
-        })
-
 #output results
 
 print("-----------------------------------------------------")
-print(f"STOCK SYMBOL: {symbol}")
-print("RUN AT: " + datetime.datetime.now().strftime("%Y-%m-%d %H:%m:%S")) #use date time module
-print("-----------------------------------------------------")
-print(f"LATEST DAY OF AVAILABLE DATA: {last_refreshed}")
-print(f"LATEST DAILY CLOSING PRICE: {to_usd(float(latest_close_usd))}")
-print(f"RECENT HIGH: {to_usd(float(recent_high))}") 
-print(f"RECENT LOW: {to_usd(float(recent_low))}") 
-print("-----------------------------------------------------")
+print(f"Stock Symbol: {user_input}")
+print("Run at: " + datetime.datetime.now().strftime("%Y-%m-%d %H:%m:%S")) #use date time module
+print(" ")
+print(f"Latest day of available data: {last_refreshed}")
+print(f"Latest daily closing price: {to_usd(float(latest_close_usd))}")
+print(f"Recent high: {to_usd(float(recent_high))}") 
+print(f"Recent low: {to_usd(float(recent_low))}") 
+print(" ")
 
-#calculating risk
+#making a recommendation, redid risk
+average_high_price = ((sum(high_prices))/(len(high_prices)))
+formatted_average_high = to_usd(average_high_price)
 
-risk_range = float(daily_prices["2. high"]) - float(daily_prices["3. low"])
-print("-----------------------------------------------------")
-print("Risk Explanation:")
-print("Please specify the amount of risk you are willing to accept by inputting a number (0-100) that represents a decrease from the stock high. For example, 10 means that you are ok with the stock losing 10 percent of its value from the high.")
-print("-----------------------------------------------------")
+print("Recommendation:")
+if float(latest_close_usd) > float(average_high_price):
+    print(f"You should NOT buy the stock; the current close price {to_usd(float(latest_close_usd))}" + " is more than the average high " + str(formatted_average_high) + ". Wait until the price goes down and the stock becomes more affordable.")
+else:
+    print(f"You should BUY the stock; the current close price {to_usd(float(latest_close_usd))}" + " is less than the average high (" + str(formatted_average_high) + "). The stock should go up in value. Get it now before it does!")
 
-risk_input = input("Risk input:")
-risk_input_percent = float(risk_input) / 100
-stock_drop = float(daily_prices["2. high"]) * (1-risk_input_percent)
-risk_span_top = to_usd(float(daily_prices["2. high"]))
-risk_span_bottom = to_usd(float(stock_drop))
-
-
-if risk_input != 0 < float(risk_input) <100.000001:
-        if float(daily_prices["3. low"]) < float(stock_drop):
-                print(f"Your risk span is: {risk_span_bottom} to {risk_span_top}") 
-                print(f"Latest price: {to_usd(float(latest_close_usd))}") 
-                print ("RECOMMENDATION: Don't Buy!")
-                print ("RECOMMENDATION REASON: Because the latest closing price is not within threshold of your risk tolerance, don't buy.")
-                print("-----------------------------------------------------")
-                
-        else:
-                print(f"Your risk span is: {risk_span_bottom} to {risk_span_top}") 
-                print(f"Latest price: {to_usd(float(latest_close_usd))}") 
-                print("RECOMMENDATION: Buy!")
-                print ("RECOMMENDATION REASON: Because the latest closing price is within threshold of your risk tolerance, buy.")
-                print("-----------------------------------------------------")
-else: 
-        print ("Oh no! that's not a valid risk, the program will now close")
-        print("-----------------------------------------------------")  
 exit()    
 
-print("-----------------------------------------------------")
-print("WRITING DATA TO CSV: {csv_file_path}")
-print("-----------------------------------------------------")
-
-parsed_response["Meta Data"].keys()
-
+print("Writing data to csv: {csv_file_path}")
