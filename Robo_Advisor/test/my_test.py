13a9c0fd5888bf422eb49da9bc57417bcd511bdd
SarahPavlak <46604461+SarahPavlak@users.noreply.github.com>
2019-05-10 16:58:37 -0400

    Update my_test.py

--- Robo_Advisor/test/my_test.py
+++ Robo_Advisor/test/my_test.py
@@ -1,4 +1,14 @@
-from app.my_robo_test import to_usd, compile_url
+#My Tests
+import csv 
+import json
+import requests
+import os 
+from dotenv import load_dotenv
+import datetime
+import pandas as pd 
+from app.my_robo_test import to_usd, compile_url, write_to_csv_header
+
+load_dotenv()
 
 def test_to_usd():
     result = to_usd(4)
@@ -14,3 +24,9 @@ def test_compile_url():
     result = compile_url()
     assert result == "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=API_KEY"
 
+def test_write_to_csv_header():
+    result = write_to_csv_header()
+    csv_file_path = ("/Users/SarahPavlak/Desktop/Final_Revamps-master/Robo_Advisor/app/data/prices_AAPL.csv")
+    with open(csv_file_path, "r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        assert result == ['timestamp', 'open', 'low', 'high', 'close', 'volume']
